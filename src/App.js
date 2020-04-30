23571a2726b45097070a78bf6872223ab4db6b48
sanniabdullateef <slateef39@gmail.com>
2020-04-30 21:46:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import { fetchData } from '../src/api'
+// import Card from './component/Card/Card';
+// import Charts from './component/Chart/Charts';
+// import CountryPicker from './component/CountryPicker/CountryPicker'
+import { Card, Charts, CountryPicker } from './component'
+import covid19Images from './images/image.png'
 
-function App() {
+
+ class App extends React.Component {
+   state = {
+      data: {},
+      country: '',
+   }
+
+  async componentDidMount () {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData })
+
+    //console.log(data)
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    // console.log(fetchedData)
+    // fetch the data
+    // set the state
+    this.setState({ data: fetchedData, country: country })
+  }
+
+  render() {
+    const { data, country } = this.state
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <img className={styles.image} src={covid19Images} alt="covid-19" />
+      <Card data= {data}/>
+      <CountryPicker handleCountryChange={this.handleCountryChange} />
+      <Charts data={data} country={country} />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/component/index.js
@@ -0,0 +1,3 @@
+export { default as Card } from './Card/Cards';
+export { default as Charts } from './Chart/Charts'
+export { default as CountryPicker } from './CountryPicker/CountryPicker'
